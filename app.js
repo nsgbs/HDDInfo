adb00720722cd70f5720c53a3b0ea928f02933b6
Nathan Gomes <56990557+nsgbs@users.noreply.github.com>
2023-07-20 17:41:17 -0300

    First usable version

--- app.js
+++ app.js
@@ -1,14 +1,87 @@
-const { exec } = require('child_process');
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const { promisify } = require('util');
+const XmlReader = require('xml-reader');
+const pathXml = (__dirname + '/report.xml');
+const xmlQuery = require('xml-query');
+const readFileAsync = promisify(fs.readFile);
+const exec = promisify(require('child_process').exec);
+
 
 app.use(express.static('public'));
 
-app.get('/', (req,res) =>{
-  //res.send("Funcionando topperson. testando o nodemon");
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/config', (req, res) => {
+  res.sendFile(__dirname + '/config.html');
+});
+
+app.get('/getNewInfo', (req, res) => {
+  console.log("parsedInfo");
+  getInfoHDDsDEBUG().then(parsedInfo => {
+    res.send(parsedInfo)
+  });
+});
+
 
 app.listen(3000, () => {
   console.log("server exec on port 3000");
 });
+
+async function getInfoHDDs() {
+  try {
+    const { stdout, stderr } = await exec(__dirname + '/HDSentinel -xml -dump', { encoding: "UTF-8" });
+    let data = stdout;
+    let parsedInfo = {};
+    xmlQuery(XmlReader.parseSync(data)).find("Application_Information").children().each(node => {
+      parsedInfo[node.name] = xmlQuery(node).children().text();
+    });
+    xmlQuery(XmlReader.parseSync(data)).find("Computer_Information").children().each(node => {
+      parsedInfo[node.name] = xmlQuery(node).children().text();
+    });
+    //get hdds quantity
+    parsedInfo.HDDQUANTITY = xmlQuery(XmlReader.parseSync(data)).find("Hard_Disk_Sentinel").children().size() - 2;
+    for (let i = 0; i < parsedInfo.HDDQUANTITY; i++) {
+      parsedInfo["Physical_Disk_Information_Disk_" + i] = {};
+      xmlQuery(XmlReader.parseSync(data)).find("Physical_Disk_Information_Disk_" + i).children().each(node => {
+        //creating indexes
+        parsedInfo["Physical_Disk_Information_Disk_" + i][node.name] = {};
+        xmlQuery(node).children().each(subnode => {
+          parsedInfo["Physical_Disk_Information_Disk_" + i][node.name][subnode.name] = xmlQuery(subnode).children().text();
+        })
+      });
+    };
+    return parsedInfo;
+  } catch (e) {
+    console.error(e); // should contain code (exit code) and signal (that caused the termination).
+  }
+}
+
+//use this function to simulate executing HDSentinel binaries for develop/debug
+//to use this function, generate a .xml report on HDSentinel using -xml option
+async function getInfoHDDsDEBUG() {
+  let parsedInfo = {};
+  const data = await readFileAsync(pathXml, 'utf-8');
+    xmlQuery(XmlReader.parseSync(data)).find("Application_Information").children().each(node => {
+      parsedInfo[node.name] = xmlQuery(node).children().text();
+    });
+    xmlQuery(XmlReader.parseSync(data)).find("Computer_Information").children().each(node => {
+      parsedInfo[node.name] = xmlQuery(node).children().text();
+    });
+    //get hdds quantity
+    parsedInfo.HDDQUANTITY = xmlQuery(XmlReader.parseSync(data)).find("Hard_Disk_Sentinel").children().size() - 2;
+    for (let i = 0; i < parsedInfo.HDDQUANTITY; i++) {
+      parsedInfo["Physical_Disk_Information_Disk_" + i] = {};
+      xmlQuery(XmlReader.parseSync(data)).find("Physical_Disk_Information_Disk_" + i).children().each(node => {
+        //creating indexes
+        parsedInfo["Physical_Disk_Information_Disk_" + i][node.name] = {};
+        xmlQuery(node).children().each(subnode => {
+          parsedInfo["Physical_Disk_Information_Disk_" + i][node.name][subnode.name] = xmlQuery(subnode).children().text();
+        })
+      });
+    }
+  return parsedInfo;
+}
---
+++ public/loadInfo.js
@@ -0,0 +1,73 @@
+const botaoGetInfo = document.getElementById('botaoGetInfo');
+const sysMainStatusDiv = document.getElementById('systemMainStatusDiv');
+const sysMainStatusDivData = document.getElementById('systemMainStatusDivData');
+const listDevices = document.getElementById('listDevices');
+//let sysMainInfoP = document.createElement('p');
+//sysMainInfoP.setAttribute('class', 'topMain-paragraph');
+//const devicesMainContainer = document.createElement('div');
+//devicesMainContainer.setAttribute('class', 'devicesMainContainer');
+//const devicesMain = document.createElement('div');
+//devicesMain.setAttribute('class', 'devicesMain')
+//const devicesMainP = document.createElement('p');
+
+function addParagraphsToDiv(divToAdd, innerHtmlData){
+    let sysMainInfoP = document.createElement('p');
+    sysMainInfoP.setAttribute('class', 'topMain-paragraph');
+    sysMainInfoP.innerHTML = innerHtmlData;
+    divToAdd.appendChild(sysMainInfoP);
+}
+
+function addDevicesToDiv(divDevices, innerHtmlData=[]){
+    let devicesMainContainer = document.createElement('div');
+    devicesMainContainer.setAttribute('class', 'devicesMainContainer');
+    let devicesMain = document.createElement('div');
+    devicesMain.setAttribute('class', 'devicesMain');
+    let devicesMainP = document.createElement('p');
+    innerHtmlData.forEach((value) => {
+        devicesMainP = document.createElement('p');
+        devicesMainP.innerHTML = value;
+        devicesMain.appendChild(devicesMainP);
+    });
+    devicesMainContainer.appendChild(devicesMain);
+    divDevices.appendChild(devicesMainContainer);
+}
+
+
+botaoGetInfo.addEventListener('click', () => {
+    fetch('/getNewInfo')
+        .then(response => response.json())
+        .then(data => {
+            //clear old info
+            sysMainStatusDivData.replaceChildren();
+            listDevices.replaceChildren();
+
+            //add system data
+            addParagraphsToDiv(sysMainStatusDivData, `Versão instalada: ${data.Installed_version}`);
+            addParagraphsToDiv(sysMainStatusDivData, `Report criado em: ${data.Current_Date_And_Time}`);
+            addParagraphsToDiv(sysMainStatusDivData, `Tempo de criação: ${data.Report_Creation_Time}`);
+            addParagraphsToDiv(sysMainStatusDivData, `Nome do sistema: ${data.Computer_Name}`);
+
+            //signature
+            let sysMainInfoP = document.createElement('p');
+            sysMainInfoP.setAttribute('class', 'bottom-paragraph');
+            sysMainInfoP.innerHTML = 'Developed by NSG';
+            sysMainStatusDivData.appendChild(sysMainInfoP);
+            
+            //add devices
+            for (let i=0; i < data.HDDQUANTITY; i++){
+                console.log("for cycle devices");
+                addDevicesToDiv(listDevices, [
+                    `HDD num: ${data['Physical_Disk_Information_Disk_' + i]['Hard_Disk_Summary']['Hard_Disk_Number']}`,
+                    `Hard_Disk_Device: ${data['Physical_Disk_Information_Disk_' + i]['Hard_Disk_Summary']['Hard_Disk_Device']}`,
+                    `Hard_Disk_Model_ID: ${data['Physical_Disk_Information_Disk_' + i]['Hard_Disk_Summary']['Hard_Disk_Model_ID']}`,
+                    `Health: ${data['Physical_Disk_Information_Disk_' + i]['Hard_Disk_Summary']['Health']}`,
+                    `Performance: ${data['Physical_Disk_Information_Disk_' + i]['Hard_Disk_Summary']['Performance']}`,
+                    `Description: ${data['Physical_Disk_Information_Disk_' + i]['Hard_Disk_Summary']['Description']}`,
+                    `Action: ${data['Physical_Disk_Information_Disk_' + i]['Hard_Disk_Summary']['Tip']}`
+                ])
+            }
+
+            //debug
+            console.log(data)
+        });
+});
