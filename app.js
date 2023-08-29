af89409562ee1c142ad601a79c2b989ba4d43d1b
Nathan Gomes <56990557+nsgbs@users.noreply.github.com>
2023-08-29 17:17:32 -0300

    mvc convert

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const { promisify } = require('util');
-const XmlReader = require('xml-reader');
-const pathXml = (__dirname + '/report.xml');
-const xmlQuery = require('xml-query');
-const fs1 = require('fs').promises;
 const readFileAsync = promisify(fs.readFile);
 const writeFileAsync = promisify(fs.writeFile);
-const exec = promisify(require('child_process').exec);
 const bodyParser = require('body-parser');
 let systemConfig = {};
+const routes = require('./routes')
+const path = require('path');
+
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use(routes);
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
 
+
+/*
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -35,7 +38,7 @@ app.get('/getNewInfo', (req, res) => {
     res.send(parsedInfo);
   });
 });
-
+*/
 async function saveInfoHDDs(data){
   writeFileAsync(`${__dirname}/lastInfo.json`,JSON.stringify(data),{encoding: 'utf8'});
 };
@@ -51,13 +54,14 @@ async function getLastInfoHDDs(){
   }
   return lastInfo;
 };
-
+/*
 app.get('/api/getLastInfo', (req, res) => {
   console.log("parsedLastInfo");
   getLastInfoHDDs().then(parsedInfo => {
     res.send(parsedInfo)
   });
 });
+*/
 
 async function loadConfig(){
   let parsedConfigFile;
@@ -84,14 +88,15 @@ async function saveConfig(config){
   await writeFileAsync(`${__dirname}/config.json`,JSON.stringify(jsonConfig),{encoding: 'utf8'});
 }
 
-
+/*
 app.post('/api/saveConfig', (req, res) => {
   (async function() {
     await saveConfig(req.body);
   })();
   
-
 });
+*/
+
 
 app.on('configLoaded', () => {
   app.listen(3000, () => {
@@ -100,61 +105,6 @@ app.on('configLoaded', () => {
 });
 
 
-
-async function getInfoHDDs() {
-  try {
-    const { stdout, stderr } = await exec(__dirname + '/HDSentinel -xml -dump', { encoding: "UTF-8" });
-    let data = stdout;
-    let parsedInfo = {};
-    xmlQuery(XmlReader.parseSync(data)).find("Application_Information").children().each(node => {
-      parsedInfo[node.name] = xmlQuery(node).children().text();
-    });
-    xmlQuery(XmlReader.parseSync(data)).find("Computer_Information").children().each(node => {
-      parsedInfo[node.name] = xmlQuery(node).children().text();
-    });
-    //get hdds quantity
-    parsedInfo.HDDQUANTITY = xmlQuery(XmlReader.parseSync(data)).find("Hard_Disk_Sentinel").children().size() - 2;
-    for (let i = 0; i < parsedInfo.HDDQUANTITY; i++) {
-      parsedInfo["Physical_Disk_Information_Disk_" + i] = {};
-      xmlQuery(XmlReader.parseSync(data)).find("Physical_Disk_Information_Disk_" + i).children().each(node => {
-        //creating indexes
-        parsedInfo["Physical_Disk_Information_Disk_" + i][node.name] = {};
-        xmlQuery(node).children().each(subnode => {
-          parsedInfo["Physical_Disk_Information_Disk_" + i][node.name][subnode.name] = xmlQuery(subnode).children().text();
-        })
-      });
-    };
-    return parsedInfo;
-  } catch (e) {
-    console.error(e); // should contain code (exit code) and signal (that caused the termination).
-  }
-}
-
-//use this function to simulate executing HDSentinel binaries for develop/debug
-//to use this function, generate a .xml report on HDSentinel using -xml option
-async function getInfoHDDsDEBUG() {
-  let parsedInfo = {};
-  const data = await readFileAsync(pathXml, 'utf-8');
-    xmlQuery(XmlReader.parseSync(data)).find("Application_Information").children().each(node => {
-      parsedInfo[node.name] = xmlQuery(node).children().text();
-    });
-    xmlQuery(XmlReader.parseSync(data)).find("Computer_Information").children().each(node => {
-      parsedInfo[node.name] = xmlQuery(node).children().text();
-    });
-    //get hdds quantity
-    parsedInfo.HDDQUANTITY = xmlQuery(XmlReader.parseSync(data)).find("Hard_Disk_Sentinel").children().size() - 2;
-    for (let i = 0; i < parsedInfo.HDDQUANTITY; i++) {
-      parsedInfo["Physical_Disk_Information_Disk_" + i] = {};
-      xmlQuery(XmlReader.parseSync(data)).find("Physical_Disk_Information_Disk_" + i).children().each(node => {
-        //creating indexes
-        parsedInfo["Physical_Disk_Information_Disk_" + i][node.name] = {};
-        xmlQuery(node).children().each(subnode => {
-          parsedInfo["Physical_Disk_Information_Disk_" + i][node.name][subnode.name] = xmlQuery(subnode).children().text();
-        })
-      });
-    }
-  return parsedInfo;
-}
 
 (async() => {
   systemConfig = await loadConfig();
