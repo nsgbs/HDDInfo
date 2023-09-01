96b656a5fcc99641da5ee650c113c010893fac12
Nathan Gomes <56990557+nsgbs@users.noreply.github.com>
2023-08-31 23:33:33 -0300

    clean unused code

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
-const { promisify } = require('util');
-const readFileAsync = promisify(fs.readFile);
-const writeFileAsync = promisify(fs.writeFile);
 const bodyParser = require('body-parser');
-let systemConfig = {};
 const routes = require('./routes')
 const path = require('path');
 const configManager = require('./configManager');
@@ -16,87 +11,13 @@ app.use(routes);
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
-
-/*
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/api/configInfo', (req, res) => {
-  const configToSend = req.query.configParameter;
-  res.send(systemConfig[configToSend]);
-});
-
-app.get('/config', (req, res) => {
-  res.sendFile(__dirname + '/config.html');
-});
-
-app.get('/getNewInfo', (req, res) => {
-  console.log("parsedInfo");
-  getInfoHDDsDEBUG().then(parsedInfo => {
-    saveInfoHDDs(parsedInfo);
-    res.send(parsedInfo);
-  });
-});
-*/
-// async function saveInfoHDDs(data){
-//   writeFileAsync(`${__dirname}/lastInfo.json`,JSON.stringify(data),{encoding: 'utf8'});
-// };
-
-
-/*
-app.get('/api/getLastInfo', (req, res) => {
-  console.log("parsedLastInfo");
-  getLastInfoHDDs().then(parsedInfo => {
-    res.send(parsedInfo)
-  });
-});
-
-
-async function loadConfig(){
-  let parsedConfigFile;
-  try{
-    const configFile = await readFileAsync(`${__dirname}/config.json`, {encoding: 'utf8'});
-    parsedConfigFile = JSON.parse(configFile);
-  }
-  catch(e){
-    if(e.code === 'ENOENT'){
-      parsedConfigFile = {'creationDate': Date.now()};
-    } else {
-      console.error('Loading config error. Blank config loaded instead.');
-      console.error(e);
-      parsedConfigFile = {'creationDate': Date.now()};
-    }
-  }
-  return parsedConfigFile;
-}
-
-async function saveConfig(config){
-  let jsonConfig = await loadConfig();
-  jsonConfig[Object.entries(config)[0][0]] = Object.entries(config)[0][1]
-  systemConfig[Object.entries(config)[0][0]] = Object.entries(config)[0][1]
-  await writeFileAsync(`${__dirname}/config.json`,JSON.stringify(jsonConfig),{encoding: 'utf8'});
-}
-
-
-app.post('/api/saveConfig', (req, res) => {
-  (async function() {
-    await saveConfig(req.body);
-  })();
-  
-});
-*/
-
-
 app.on('configLoaded', () => {
   app.listen(3000, () => {
     console.log("server exec on port 3000");
   })
 });
 
-
-
-(async() => {
+(async () => {
   configManager.systemConfig = await configManager.loadConfig();
   app.emit('configLoaded');
 })();
